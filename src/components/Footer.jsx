482cb6831ed9b09ed2df1a1275ceba6c9226d9f9
Macharia Elvis <machariaelvys@gmail.com>
2026-02-03 19:43:45 +0300

    icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,33 @@
 import React from "react";
 import "../styles/Footer.css";
+import instaIcon from "../assets/paintings/email.png";
+import emailIcon from "../assets/paintings/insta.png";
 
 function Footer() {
   return (
-    <footer className="footer " id="contact">
+    <footer className="footer" id="contact">
       <div className="footer-content">
         <h2 className="footer-title">JADA ART STUDIO</h2>
 
         <div className="footer-links">
-          {/* Email link */}
-          <a href="mailto:jada.artist@email.com" className="footer-link">
-            jada.artist@email.com
+          {/* Email icon */}
+          <a
+            href="mailto:jada.artist@email.com"
+            className="footer-link"
+            aria-label="Email"
+          >
+            <img src={emailIcon} alt="Email" className="footer-icon" />
           </a>
 
-          {/* Instagram link */}
+          {/* Instagram icon */}
           <a
-            href="https://www.instagram.com/sskittle_zz
-"
+            href="https://www.instagram.com/sskittle_zz"
             target="_blank"
             rel="noopener noreferrer"
             className="footer-link"
+            aria-label="Instagram"
           >
-            Instagram
+            <img src={instaIcon} alt="Instagram" className="footer-icon" />
           </a>
         </div>
 
--- src/data/artwork.js
+++ src/data/artwork.js
@@ -1,30 +1,48 @@
-import p1 from "../assets/paintings/jada.jpg";
-import p2 from "../assets/paintings/jada.jpg";
-import p3 from "../assets/paintings/jada.jpg";
-import p4 from "../assets/paintings/jada.jpg";
+import p1 from "../assets/paintings/jada1.jpg";
+import p2 from "../assets/paintings/jada2.jpg";
+import p3 from "../assets/paintings/jada3.jpg";
+import p4 from "../assets/paintings/jada4.jpg";
+import p5 from "../assets/paintings/jada5.jpg";
+import p6 from "../assets/paintings/jada6.jpg";
+
+
 
 
 const paintings = [
-  {
-    id: 1,
-    title: " ",
-    image: p1,
-  },
-  {
-    id: 2,
-    title: "",
-    image: p2,
-  },
-  {
-    id: 3,
-    title: "",
-    image: p3,
-  },
+
   {
     id: 4,
     title: "",
     image: p4,
   },
+  {
+    id: 5,
+    title: "",
+    image: p5,
+  },
+  {
+    id: 6,
+    title: "",
+    image: p6,
+  },
+ {
+    id: 3,
+    title: "",
+    image: p3,
+  },
+
+  {
+    id: 1,
+    title: " ",
+    image: p1,
+  },
+  // {
+  //   id: 2,
+  //   title: "",
+  //   image: p2,
+  // },
+  // {
+  
 
 
 
